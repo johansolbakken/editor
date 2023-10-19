d27a8a257b7e58b5dea55d2e8dbd1f5afaf27e1d
Johan Solbakken <johsol@stud.ntnu.no>
2023-10-19 18:21:00 +0200

    Move rendering code into renderer module

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
 use glyph_brush::{Section, Text};
-use wgpu::{util::DeviceExt, Backends, Instance};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
-// lib.rs
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct Vertex {
-    position: [f32; 3],
-}
-
-impl Vertex {
-    fn desc() -> wgpu::VertexBufferLayout<'static> {
-        wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[wgpu::VertexAttribute {
-                offset: 0,
-                shader_location: 0,
-                format: wgpu::VertexFormat::Float32x3,
-            }],
-        }
-    }
-}
+mod renderer;
 
 fn main() {
     // Initialize the event loop
@@ -37,126 +17,15 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
-    // Initialize WGPU
-    let instance = Instance::new(wgpu::InstanceDescriptor {
-        backends: Backends::PRIMARY,
-        ..Default::default()
-    });
-    let surface = unsafe { instance.create_surface(&window) }.unwrap();
-    let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
-        power_preference: wgpu::PowerPreference::LowPower, // or HighPerformance
-        compatible_surface: Some(&surface),
-        force_fallback_adapter: false, // Adjust as per your needs
-    }))
-    .expect("Failed to find a suitable GPU adapter");
-
-    let (device, queue) = pollster::block_on(adapter.request_device(
-        &wgpu::DeviceDescriptor {
-            label: None,
-            features: wgpu::Features::empty(),
-            limits: wgpu::Limits::default(),
-        },
-        None,
-    ))
-    .expect("Failed to create device");
-
+    let mut renderer = renderer::Renderer::new(&window);
     let mut size = window.inner_size();
-    let surface_caps = surface.get_capabilities(&adapter);
-    // Shader code in this tutorial assumes an sRGB surface texture. Using a different
-    // one will result all the colors coming out darker. If you want to support non
-    // sRGB surfaces, you'll need to account for that when drawing to the frame.
-    let surface_format = surface_caps
-        .formats
-        .iter()
-        .copied()
-        .find(|f| f.is_srgb())
-        .unwrap_or(surface_caps.formats[0]);
-    let config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: surface_format,
-        width: size.width,
-        height: size.height,
-        present_mode: surface_caps.present_modes[0],
-        alpha_mode: surface_caps.alpha_modes[0],
-        view_formats: vec![],
-    };
-    surface.configure(&device, &config);
-
-    let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
-
-    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: Some("Render Pipeline Layout"),
-        bind_group_layouts: &[],
-        push_constant_ranges: &[],
-    });
-
-    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: Some("Render Pipeline"),
-        layout: Some(&render_pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main", // 1.
-            buffers: &[
-                Vertex::desc(), // 2.
-            ], // 2.
-        },
-        fragment: Some(wgpu::FragmentState {
-            // 3.
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(wgpu::ColorTargetState {
-                // 4.
-                format: config.format,
-                blend: Some(wgpu::BlendState::REPLACE),
-                write_mask: wgpu::ColorWrites::ALL,
-            })],
-        }),
-        primitive: wgpu::PrimitiveState {
-            topology: wgpu::PrimitiveTopology::TriangleList, // 1.
-            strip_index_format: None,
-            front_face: wgpu::FrontFace::Ccw, // 2.
-            cull_mode: Some(wgpu::Face::Back),
-            // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-            polygon_mode: wgpu::PolygonMode::Fill,
-            // Requires Features::DEPTH_CLIP_CONTROL
-            unclipped_depth: false,
-            // Requires Features::CONSERVATIVE_RASTERIZATION
-            conservative: false,
-        },
-        depth_stencil: None, // 1.
-        multisample: wgpu::MultisampleState {
-            count: 1,                         // 2.
-            mask: !0,                         // 3.
-            alpha_to_coverage_enabled: false, // 4.
-        },
-        multiview: None, // 5.
-    });
-
-    // lib.rs
-    const VERTEX_DATA: &[Vertex] = &[
-        Vertex {
-            position: [-0.5, -0.5, 0.0],
-        },
-        Vertex {
-            position: [0.5, -0.5, 0.0],
-        },
-        Vertex {
-            position: [0.0, 0.5, 0.0],
-        },
-    ];
-
-    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("Vertex Buffer"),
-        contents: bytemuck::cast_slice(VERTEX_DATA),
-        usage: wgpu::BufferUsages::VERTEX,
-    });
 
     let mut staging_belt = wgpu::util::StagingBelt::new(1024);
 
     let cascadia: &[u8] = include_bytes!("font/Cascadia.ttf");
     let font = wgpu_glyph::ab_glyph::FontArc::try_from_slice(cascadia).unwrap();
     let mut glyph_brush =
-        wgpu_glyph::GlyphBrushBuilder::using_font(font).build(&device, surface_format);
+        wgpu_glyph::GlyphBrushBuilder::using_font(font).build(renderer.device(), renderer.surface_format());
 
     // Main event loop
     event_loop.run(move |event, _, control_flow| {
@@ -167,11 +36,11 @@ fn main() {
                 window.request_redraw();
             }
             Event::RedrawRequested(window_id) if window_id == window.id() => {
-                let output = surface.get_current_texture().unwrap();
+                let output = renderer.surface().get_current_texture().unwrap();
                 let view = output
                     .texture
                     .create_view(&wgpu::TextureViewDescriptor::default());
-                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                let mut encoder = renderer.device().create_command_encoder(&wgpu::CommandEncoderDescriptor {
                     label: Some("Render Encoder"),
                 });
                 {
@@ -193,8 +62,8 @@ fn main() {
                         depth_stencil_attachment: None,
                     });
 
-                    render_pass.set_pipeline(&render_pipeline);
-                    render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                    render_pass.set_pipeline(renderer.render_pipeline());
+                    render_pass.set_vertex_buffer(0, renderer.vertex_buffer().slice(..));
                     render_pass.draw(0..3, 0..1);
                 }
 
@@ -219,7 +88,7 @@ fn main() {
                 // Draw the text
                 glyph_brush
                     .draw_queued(
-                        &device,
+                        renderer.device(),
                         &mut staging_belt,
                         &mut encoder,
                         &view,
@@ -232,7 +101,7 @@ fn main() {
                 staging_belt.finish();
 
                 // submit will accept anything that implements IntoIter
-                queue.submit(std::iter::once(encoder.finish()));
+                renderer.queue().submit(std::iter::once(encoder.finish()));
                 output.present();
             }
             Event::WindowEvent {
@@ -243,9 +112,9 @@ fn main() {
                 let config = wgpu::SurfaceConfiguration {
                     width: size.width,
                     height: size.height,
-                    ..config.clone()
+                    ..renderer.surface_config().clone()
                 };
-                surface.configure(&device, &config);
+                renderer.surface().configure(renderer.device(), &config);
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
---
+++ src/renderer.rs
@@ -0,0 +1,195 @@
+use wgpu::{util::DeviceExt, Backends, Instance};
+
+// lib.rs
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[wgpu::VertexAttribute {
+                offset: 0,
+                shader_location: 0,
+                format: wgpu::VertexFormat::Float32x3,
+            }],
+        }
+    }
+}
+
+pub struct Renderer {
+    instance: wgpu::Instance,
+    surface: wgpu::Surface,
+    surface_format: wgpu::TextureFormat,
+    config: wgpu::SurfaceConfiguration,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+}
+
+impl Renderer {
+    pub fn new(window: &winit::window::Window) -> Self {
+        // Initialize WGPU
+        let instance = Instance::new(wgpu::InstanceDescriptor {
+            backends: Backends::PRIMARY,
+            ..Default::default()
+        });
+        let surface = unsafe { instance.create_surface(window) }.unwrap();
+        let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::LowPower, // or HighPerformance
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false, // Adjust as per your needs
+        }))
+        .expect("Failed to find a suitable GPU adapter");
+
+        let (device, queue) = pollster::block_on(adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+            },
+            None,
+        ))
+        .expect("Failed to create device");
+
+        let mut size = window.inner_size();
+        let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+        // one will result all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main", // 1.
+                buffers: &[
+                    Vertex::desc(), // 2.
+                ], // 2.
+            },
+            fragment: Some(wgpu::FragmentState {
+                // 3.
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1,                         // 2.
+                mask: !0,                         // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None, // 5.
+        });
+
+        // lib.rs
+        const VERTEX_DATA: &[Vertex] = &[
+            Vertex {
+                position: [-0.5, -0.5, 0.0],
+            },
+            Vertex {
+                position: [0.5, -0.5, 0.0],
+            },
+            Vertex {
+                position: [0.0, 0.5, 0.0],
+            },
+        ];
+
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTEX_DATA),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        Self {
+            instance,
+            surface,
+            surface_format,
+            config,
+            adapter,
+            device,
+            queue,
+            size,
+            render_pipeline,
+            vertex_buffer,
+        }
+    }
+
+    pub fn device(&self) -> &wgpu::Device {
+        &self.device
+    }
+
+    pub fn queue(&self) -> &wgpu::Queue {
+        &self.queue
+    }
+
+    pub fn surface_format(&self) -> wgpu::TextureFormat {
+        self.surface_format
+    }
+
+    pub fn surface(&self) -> &wgpu::Surface {
+        &self.surface
+    }
+
+    pub fn render_pipeline(&self) -> &wgpu::RenderPipeline {
+        &self.render_pipeline
+    }
+
+    pub fn vertex_buffer(&self) -> &wgpu::Buffer {
+        &self.vertex_buffer
+    }
+
+    pub fn surface_config(&self) -> &wgpu::SurfaceConfiguration {
+        &self.config
+    }
+}
