8076ef07e370006781a481f7ab6bd989e877b74f
Johan Solbakken <johsol@stud.ntnu.no>
2023-10-23 00:06:32 +0200

    Blinking cursor

--- src/text_view.cpp
+++ src/text_view.cpp
@@ -5,6 +5,9 @@
 TextView::TextView()
 {
 	m_text.emplace_back("");
+
+	m_scroll_x = -100;
+	m_scroll_y = -100;
 }
 
 void TextView::render()
@@ -21,6 +24,13 @@ void TextView::render()
 	for (const auto& line : m_text) {
 		DrawTextEx(GetFontDefault(), line.c_str(), {m_x + x - m_scroll_x, m_y + y - m_scroll_y}, font_height, font_spacing, WHITE);
 		y += font_height + line_spacing;
+	}
+
+	float rate = 0.2f;
+	float state = static_cast<float>(GetTime()) / rate;
+	if (!m_focused || sin(state) > 0) {
+		EndScissorMode();
+		return;
 	}
 
 	// cursor as "|"
