6ebd0346cb2409ab12f6fb47b30fe2acdc7b15d8
Johan Solbakken <johsol@stud.ntnu.no>
2023-10-22 22:36:53 +0200

    Switching to c

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <raylib.h>
+
+int main(void) {
+    InitWindow(800, 600, "Hello World");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello World", 10, 10, 20, DARKGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
