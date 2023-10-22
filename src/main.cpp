05b1f9ba1e359115ee601cbcd045cfd46c0629ed
Johan Solbakken <johsol@stud.ntnu.no>
2023-10-22 22:40:59 +0200

    Resizable window

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <raylib.h>
 
 int main(void) {
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
     InitWindow(800, 600, "Hello World");
+
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello World", 10, 10, 20, DARKGRAY);
+        ClearBackground((Color){0,0,0,255});
+        DrawText("Hello World", 10, 10, 20, (Color){255,255,255,255});
         EndDrawing();
     }
     CloseWindow();
